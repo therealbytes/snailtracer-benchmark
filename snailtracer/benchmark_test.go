a3bd44b7663b1ef94023ea615ff531888158841d
bytes <therealbyt3s@gmail.com>
2023-08-24 21:06:09 +0200

    refactor: clean

--- snailtracer/benchmark_test.go
+++ snailtracer/benchmark_test.go
@@ -14,49 +14,14 @@ import (
 )
 
 func TestNativeSnailtracer(b *testing.T) {
-	s := newScene(1024, 768)
-
-	s.deltaX = &Vector{s.width * 513500 / s.height, 0, 0}
-	s.deltaY = s.deltaX.cross(s.camera.direction).norm().scaleMul(513500).scaleDiv(1000000)
-
-	s.spheres = []*Sphere{
-		{100000000000, &Vector{100001000000, 40800000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 250000, 250000}, DiffuseMaterial},
-		{100000000000, &Vector{-99901000000, 40800000, 81600000}, &Vector{0, 0, 0}, &Vector{250000, 250000, 750000}, DiffuseMaterial},
-		{100000000000, &Vector{50000000, 40800000, 100000000000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
-		{100000000000, &Vector{50000000, 40800000, -99830000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, DiffuseMaterial},
-		{100000000000, &Vector{50000000, 100000000000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
-		{100000000000, &Vector{50000000, -99918400000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
-		{16500000, &Vector{27000000, 16500000, 47000000}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{600000000, &Vector{50000000, 681330000, 81600000}, &Vector{12000000, 12000000, 12000000}, &Vector{0, 0, 0}, DiffuseMaterial},
-	}
-
-	s.triangles = []*Triangle{
-		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{73000000, 49500000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 0, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{89500000, 25740000, 78000000}, &Vector{73000000, 49500000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{89500000, 23760000, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{73000000, 0, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 49500000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{73000000, 0, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{89500000, 25740000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{73000000, 49500000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{89500000, 23760000, 78000000}, &Vector{73000000, 0, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{89500000, 25740000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 25740000, 78000000}, &Vector{89500000, 25740000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 23760000, 78000000}, &Vector{89500000, 23760000, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{89500000, 23760000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
-	}
-
-	// Calculate all the triangle surface normals
-	for i := range s.triangles {
-		tri := s.triangles[i]
-		tri.normal = tri.b.sub(tri.a).cross(tri.c.sub(tri.a)).norm()
-	}
+	s := NewBenchmarkScene()
 
 	// Trace a few pixels and collect their colors (sanity check)
 	color := &Vector{0, 0, 0}
 
-	// color = color.add(s.trace(512, 384, 1)) // Flat diffuse surface, opposite wall
-	color = color.add(s.trace(512, 384, 8)) // Flat diffuse surface, opposite wall
-	color = color.add(s.trace(325, 540, 8)) // Reflective surface mirroring left wall
+	color = color.add(s.trace(512, 384, 1)) // Flat diffuse surface, opposite wall
+	// color = color.add(s.trace(512, 384, 8)) // Flat diffuse surface, opposite wall
+	// color = color.add(s.trace(325, 540, 8)) // Reflective surface mirroring left wall
 	// color = color.add(s.trace(600, 600, 8)) // Refractive surface reflecting right wall
 	// color = color.add(s.trace(522, 524, 8)) // Reflective surface mirroring the refractive surface reflecting the light
 	color = color.scaleDiv(4)
--- snailtracer/tracer.go
+++ snailtracer/tracer.go
@@ -353,3 +353,48 @@ func (s *Scene) traceRay(ray *Ray) (int, Primitive, int) {
 
 	return dist, p, id
 }
+
+func (s *Scene) TracePixel(x, y, spp int) *Vector {
+	return s.trace(x, y, spp)
+}
+
+func NewBenchmarkScene() *Scene {
+	s := newScene(1024, 768)
+
+	s.deltaX = &Vector{s.width * 513500 / s.height, 0, 0}
+	s.deltaY = s.deltaX.cross(s.camera.direction).norm().scaleMul(513500).scaleDiv(1000000)
+
+	s.spheres = []*Sphere{
+		{100000000000, &Vector{100001000000, 40800000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 250000, 250000}, DiffuseMaterial},
+		{100000000000, &Vector{-99901000000, 40800000, 81600000}, &Vector{0, 0, 0}, &Vector{250000, 250000, 750000}, DiffuseMaterial},
+		{100000000000, &Vector{50000000, 40800000, 100000000000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
+		{100000000000, &Vector{50000000, 40800000, -99830000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, DiffuseMaterial},
+		{100000000000, &Vector{50000000, 100000000000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
+		{100000000000, &Vector{50000000, -99918400000, 81600000}, &Vector{0, 0, 0}, &Vector{750000, 750000, 750000}, DiffuseMaterial},
+		{16500000, &Vector{27000000, 16500000, 47000000}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{600000000, &Vector{50000000, 681330000, 81600000}, &Vector{12000000, 12000000, 12000000}, &Vector{0, 0, 0}, DiffuseMaterial},
+	}
+
+	s.triangles = []*Triangle{
+		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{73000000, 49500000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 0, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{89500000, 25740000, 78000000}, &Vector{73000000, 49500000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{89500000, 23760000, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{73000000, 0, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 49500000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{73000000, 0, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{89500000, 25740000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{73000000, 49500000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{89500000, 23760000, 78000000}, &Vector{73000000, 0, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 25740000, 78000000}, &Vector{73000000, 25740000, 61500000}, &Vector{89500000, 25740000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 25740000, 78000000}, &Vector{89500000, 25740000, 78000000}, &Vector{73000000, 25740000, 94500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 23760000, 78000000}, &Vector{89500000, 23760000, 78000000}, &Vector{73000000, 23760000, 61500000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+		{&Vector{56500000, 23760000, 78000000}, &Vector{73000000, 23760000, 94500000}, &Vector{89500000, 23760000, 78000000}, &Vector{0, 0, 0}, &Vector{0, 0, 0}, &Vector{999000, 999000, 999000}, SpecularMaterial},
+	}
+
+	// Calculate all the triangle surface normals
+	for i := range s.triangles {
+		tri := s.triangles[i]
+		tri.normal = tri.b.sub(tri.a).cross(tri.c.sub(tri.a)).norm()
+	}
+
+	return s
+}
